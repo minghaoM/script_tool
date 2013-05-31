489bc3b02fe263cdbb7d5410f180a8cad3a784c8
minghaoM <maminghao@gmail.com>
2013-05-30 22:06:01 -0400

    add subfolder for each tool, add cleaner

---
+++ cleaner/cleaner.py
@@ -0,0 +1,72 @@
+﻿"""
+It is commonly used for cleanning daily build files
+"""
+
+import logging
+import os
+import shutil
+import datetime
+
+DAYFROM = 9
+DAYTO = 14
+PATHLIST = [
+r"D:\installers\version1001\product",
+r"D:\installers\version1002\product"
+]
+
+def get_dateList(startDayCount, endDayCount):
+    '''
+    Get date list from today
+    '''
+    today = datetime.date.today()
+    return [(today - datetime.timedelta(dayCount)) for dayCount in range(startDayCount, endDayCount)]
+
+def get_subfolderList(path, startDayCount, endDayCount):
+    '''
+    Get subfolder list depend on date period
+    '''
+    allSubPaths = [os.path.join(path, pathName) for pathName in os.listdir(path)]
+    dateList = get_dateList(startDayCount, endDayCount)
+    return list(filter(lambda x: is_validDatePath(x, dateList), allSubPaths))
+    
+def is_validDatePath(path, dates):
+    postfix1s = [date.strftime("AnnualLicense(%m.%d)") for date in dates]
+    postfix2s = [date.strftime("%m.%d.%Y") for date in dates]
+    postfix3s = [date.strftime("%Y.%m.%d") for date in dates]
+    postfixs = []
+    postfixs.extend(postfix1s)
+    postfixs.extend(postfix2s)
+    postfixs.extend(postfix3s)
+    if any(map(path.endswith, postfixs)):
+        return True
+    else:
+        return False
+
+def clean_path(path):
+    if os.path.isfile(path):
+        try:
+            os.remove(path)
+            logging.info("file {0} is removed".format(path))
+        except:
+            logging.error("remove file {0} fail!".format(path))
+    elif os.path.isdir(path):
+        try:
+            shutil.rmtree(path)
+            logging.info("all files in {0} is removed".format(path))
+        except:
+            logging.error("remove directory {0} fail!".format(path))
+    else:
+        logging.error("invalid path! {0}".format(path))
+
+def run(pathList, dayFrom, dayTo):
+    for path in pathList:
+        #get subpath list
+        cleanList = get_subFolderList(path, dayFrom, dayTo)
+        #clean
+        for targetPath in cleanList:
+            clean_path(targetPath)
+            
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s %(message)s', filename='logkey-{0}.log'.format(get_dateString()),
+        level=logging.DEBUG)
+    run(PATHLIST, DAYFROM, DAYTO)
---
+++ cleaner/test.py
@@ -0,0 +1,57 @@
+﻿import datetime
+import os
+import shutil
+import unittest
+import cleaner
+
+
+class Test_common_func(unittest.TestCase):
+    
+    def setUp(self):
+        self.today = datetime.date.today()
+        self.path = r"c:\test"
+        
+    def test_get_dateList(self):
+        day1 = self.today - datetime.timedelta(days=1)
+        day2 = self.today - datetime.timedelta(days=2)
+        self.assertEqual(cleaner.get_dateList(1,3),[day1, day2])
+        
+        
+    def test_is_validDatePath(self):
+        testPath = os.path.join(self.path, self.today.strftime("%Y.%m.%d"))
+        testDates = [self.today]
+        self.assertTrue(cleaner.is_validDatePath(testPath, testDates))
+        
+class Test_io_func(unittest.TestCase):
+    
+    def setUp(self):
+        self.today = datetime.date.today()
+        self.resource = r"C:\testresource"#there are some test data in it
+        self.path = r"c:\test"
+        self.tmpPath = os.path.join(self.path, "path")
+        if not os.path.isdir(self.path):
+            os.mkdir(self.path)
+        shutil.copytree(self.resource, self.tmpPath)
+
+    def tearDown(self):
+        shutil.rmtree(os.path.join(self.path, "path"))
+
+    def test_get_subfolderList(self):
+        dateString1 = (self.today - datetime.timedelta(days=1)).strftime("%Y.%m.%d")
+        dateString2 = (self.today - datetime.timedelta(days=2)).strftime("%Y.%m.%d")
+        subFolder1 = os.path.join(self.tmpPath, dateString1)
+        subFolder2 = os.path.join(self.tmpPath, dateString2)
+        self.assertEqual(cleaner.get_subfolderList(self.tmpPath, 1, 3).sort(), [subFolder1, subFolder2].sort())
+    
+    def test_clean_path(self):
+        testPath = os.path.join(self.tmpPath, "delpath")
+        testFile = os.path.join(self.tmpPath, "delfile.txt")
+        self.assertTrue(os.path.isdir(testPath))
+        self.assertTrue(os.path.isfile(testFile))
+        cleaner.clean_path(testPath)
+        self.assertFalse(os.path.isdir(testPath))
+        cleaner.clean_path(testFile)
+        self.assertFalse(os.path.isdir(testFile))
+    
+if __name__ == "__main__":
+    unittest.main()
